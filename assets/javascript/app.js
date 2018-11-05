b1c0e49f12cde35f82b96e5b5cafe66eea9cb461
LidiaWebDev <lidia@fishisfast.com>
2018-11-05 11:15:42 -0800

    changed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,8 @@
     "New York",
     "France"
   ]);
-userAnswer = [];
+var sec = timer;
+// userAnswer = [];
 
 $(".maincontainer").hide();
 $(".results").hide();
@@ -29,145 +30,154 @@ $("#start").on("click", function(event) {
   return;
 });
 
+startCounting = function(sec) {
+  var sec = timer;
+  setInterval(function(timer) {
+    document.getElementById("timer").innerHTML = sec;
+    sec--;
+    if (sec === -2) {
+      clearInterval(timer);
+      $(".maincontainer").hide();
+      document.getElementById("timer").innerHTML =
+        "<h2>Check the Results!</h2>";
+      //    
+      $(".results").show();
+      collectResults();
+    }
+  }, 1000);
+};
+
 $("#complete").on("click", function(event) {
+  clearInterval(sec);
   event.preventDefault();
   $("#intro").hide();
   $(".maincontainer").hide();
   $(".results").show();
-  // stopCounting();
+  finishCounting();
   return;
 });
 
-startCounting = function(sec) {
-  var sec = timer;
-  setInterval(function() {
-    document.getElementById("timer").innerHTML = " " + sec;
-    if (sec <= -2) {
-      clearTimeout(timer);
-      $(".maincontainer").hide();
-      Element.innerHTML = "<h2>Check the Results!</h2>";
-      $(".results").show();
-      collectResults();
-    }
-    sec--;
-  }, 1000);
+finishCounting = function() {
+  timer = 0;
+  clearInterval(sec);
+  console.log("what");
 };
-
-// for (var i = 0; i < answers.length; i++) {
-//   if ($('input[name="radio"]:checked').val() == answers) {
-//     right++;
-//   } else {
-//     wrong++;
-//   }
-//console.log(correct);
-//console.log(incorrect);
-// }
-
-function collectResults() {
-  var Q1 = $('input:radio[name="q1"]:checked').val();
-  var Q2 = $('input:radio[name="q2"]:checked').val();
-  var Q3 = $('input:radio[name="q3"]:checked').val();
-  var Q4 = $('input:radio[name="q4"]:checked').val();
-  var Q5 = $('input:radio[name="q5"]:checked').val();
-  var Q6 = $('input:radio[name="q6"]:checked').val();
-  var Q7 = $('input:radio[name="q7"]:checked').val();
-  var Q8 = $('input:radio[name="q8"]:checked').val();
-  var Q9 = $('input:radio[name="q9"]:checked').val();
-  var Q10 = $('input:radio[name="q10"]:checked').val();
-
-  for (var i = 0; i < answers.length; i++) {
-    if (Q1 == undefined) {
-      unanswered++;
-    } else if (Q1 == "George Washington") {
+rightAnswers = 0;
+for (var i = 0; i <= 10; i++) {
+  var radios = document.getElementsByName("q" + i);
+  for (var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if (radio.value === "right" && radio.checked) {
       rightAnswers++;
-    } else {
-      wrongAnswers++;
     }
+  }
 
-    if (Q2 == undefined) {
-      unanswered++;
-    } else if (Q2 == "1789 ") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
+  function collectResults() {
+    var Q1 = $('input:radio[name="q1"]:checked').val();
+    var Q2 = $('input:radio[name="q2"]:checked').val();
+    var Q3 = $('input:radio[name="q3"]:checked').val();
+    var Q4 = $('input:radio[name="q4"]:checked').val();
+    var Q5 = $('input:radio[name="q5"]:checked').val();
+    var Q6 = $('input:radio[name="q6"]:checked').val();
+    var Q7 = $('input:radio[name="q7"]:checked').val();
+    var Q8 = $('input:radio[name="q8"]:checked').val();
+    var Q9 = $('input:radio[name="q9"]:checked').val();
+    var Q10 = $('input:radio[name="q10"]:checked').val();
+
+    for (var i = 0; i < answers.length; i++) {
+      if (Q1 == undefined) {
+        unanswered++;
+      } else if (Q1 == "George Washington") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q2 == undefined) {
+        unanswered++;
+      } else if (Q2 == "1789 ") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q3 == undefined) {
+        unanswered++;
+      } else if (Q3 == "Fifty states of the United States") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q4 == undefined) {
+        unanswered++;
+      } else if (Q4 == "Edsel") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q5 == undefined) {
+        unanswered++;
+      } else if (Q5 == "Thomas Edison") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q6 == undefined) {
+        unanswered++;
+      } else if (Q6 == "Christopher Columbus") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q7 == undefined) {
+        unanswered++;
+      } else if (Q7 == "1850") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q8 == undefined) {
+        unanswered++;
+      } else if (Q8 == "Michael Jackson") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q9 == undefined) {
+        unanswered++;
+      } else if (Q9 == "New York") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      if (Q10 == undefined) {
+        unanswered++;
+      } else if (Q10 == "France") {
+        rightAnswers++;
+      } else {
+        wrongAnswers++;
+      }
+
+      // function printOnTheScreen() {
+      //   document.getElementById("right").innerHTML =
+      //     "Right Answers: " + rightAnswers;
+      //   document.getElementById("wrong").innerHTML =
+      //     "Wrong Answers: " + wrongAnswers;
+      //   document.getElementById("una").innerHTML = "Unanswered: " + unanswered;
+      // }
+
+      $("#right").html(rightAnswers);
+      $("#wrong").html(wrongAnswers);
+      $("#unanswered").html(unanswered);
+      $(".results").show();
     }
-
-    if (Q3 == undefined) {
-      unanswered++;
-    } else if (Q3 == "Fifty states of the United States") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q4 == undefined) {
-      unanswered++;
-    } else if (Q4 == "Edsel") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q5 == undefined) {
-      unanswered++;
-    } else if (Q5 == "Thomas Edison") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q6 == undefined) {
-      unanswered++;
-    } else if (Q6 == "Christopher Columbus") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q7 == undefined) {
-      unanswered++;
-    } else if (Q7 == "1850") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q8 == undefined) {
-      unanswered++;
-    } else if (Q8 == "Michael Jackson") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q9 == undefined) {
-      unanswered++;
-    } else if (Q9 == "New York") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    if (Q10 == undefined) {
-      unanswered++;
-    } else if (Q10 == "France") {
-      rightAnswers++;
-    } else {
-      wrongAnswers++;
-    }
-
-    // function printOnTheScreen() {
-    //   document.getElementById("right").innerHTML =
-    //     "Right Answers: " + rightAnswers;
-    //   document.getElementById("wrong").innerHTML =
-    //     "Wrong Answers: " + wrongAnswers;
-    //   document.getElementById("una").innerHTML = "Unanswered: " + unanswered;
-    // }
-
-    $("#right").html(rightAnswers);
-    $("#wrong").html(wrongAnswers);
-    $("#unanswered").html(unanswered);
-    $(".results").show();
   }
 }
 
